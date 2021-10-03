7f6ec43c3c702b74d490d47bfb6682a642ab4c97
Your Name <you@example.com>
2021-10-04 03:48:47 +0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,10 @@
+//create an array like so:
+// var colors = ["red","blue","green"];
+
+//you can loop through an array like this:
+// for (var i = 0; i < colors.length; i++) {
+//     console.log(colors[i]);
+// }
+
+
+console.log('mahbub re maro')
